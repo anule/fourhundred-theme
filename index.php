ed4a4e73d7a74bae1fb29b286e0b962ae7ddb4f6
anule <anule.ndukwu@gmail.com>
2018-04-20 15:39:39 -0400

    create basic theme

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>400 North Medical</title>
+    <link href="<?php echo get_bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <!-- navigation header-->
+    <div class='container'>
+    <header>
+      <a href="index.html"><img src="#" alt="400 North Medical Center logo"></a>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Doctors &amp; Staff</a></li>
+          <li><a href="#">Sleep Health Center</a></li>
+          <li><a href="#">Research Clinic</a></li>
+          <li><a href="#">Tests and Services</a></li>
+          <li><a href="#">Payment</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- ** MAIN SECTION ** -->
+    <main>
+    <!-- These images will be a carousel slideshow using slick -->
+    <div class="jumbotron">
+        <!-- TODO: wrap these images in an image div -->
+        <img src="#" alt="400 North from the east">
+        <img src="#" alt="400 North from the west">
+        <img src="#" alt="400 North front entrace">
+        <img src="#" alt="400 North front desk">
+        <img src="#" alt="400 North nursing desk">
+        <img src="#" alt="A room at the Sleep Health Center">
+        <img src="#" alt="400 North staff picture">
+    </div>
+
+    <!-- ** Individual Page ** -->
+    <section>
+        <div id='staff portrait'>A picture of a staff member</div>
+        <h1>Staff Member Name</h1>
+        <div class="bio"><p>We all dined together, and I could see that poor Art was trying to make the best of things. Van Helsing had been silent all dinner-time; but when we had lit our cigars he said--</p>
+        <p>"Call me what you will," he said. "I hope I may always have the title of a friend. And let me say that I am at a loss for words to thank you for your goodness to my poor dear." He paused a moment, and went on: "I know that she understood your goodness even better than I do; and if I was rude or in any way wanting at that time you acted so--you remember"--the Professor nodded--"you must forgive me."</p>
+        </div>
+    </section>
+
+    <!-- ** Contact ** -->
+    <section>
+        <h1>Contact page</h1>
+        <div>This is a map that is embedded</div>
+        <div>List of numbers for the doctors</div>
+    </section>
+
+    <!-- ** Text Page ** -->
+    <section>
+        <h1>Text Heavy Page</h1>
+        <div>This is a text heavy page with lots of words</div>
+        <div><p>"I know it was hard for you to quite trust me then, for to trust such violence needs to understand; and I take it that you do not--that you cannot--trust me now, for you do not yet understand. And there may be more times when I shall want you to trust when you cannot--and may not--and must not yet understand. But the time will come when your trust shall be whole and complete in me, and when you shall understand as though the sunlight himself shone through. Then you shall bless me from first to last for your own sake, and for the sake of others and for her dear sake to whom I swore to protect."</p>
+        <p>"And, indeed, indeed, sir," said Arthur warmly, "I shall in all ways trust you. I know and believe you have a very noble heart, and you are Jack's friend, and you were hers. You shall do what you like."</p></div>
+    </section>
+
+    </main>
+
+    <footer> <!-- ** START FOOTER SECTION ** -->
+      <span>Copyright 400 North Medical Center</span>
+    </footer> <!-- ** END FOOTER SECTION ** -->
+</body>
+</html>
