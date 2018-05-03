0a86b7368bc7e16bd4d455ef552c143e9f7758d8
anule <anule.ndukwu@gmail.com>
2018-05-03 15:40:06 -0400

    remove scripts from header/footer.php

--- header.php
+++ header.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>400 North Medical</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <link href="<?php echo get_bloginfo('template_directory'); ?>/normalize.css" rel="stylesheet">
-    <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+
     <?php wp_head();?>
   </head>
 
