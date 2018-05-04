0448f37de0f7ade3ba4a65b6b7b2e6430ea308a0
anule <anule.ndukwu@gmail.com>
2018-05-04 16:42:40 -0400

    header updates title dynamically

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>400 North Medical</title>
+    <title><?php wp_title( '|', true, 'right' ); echo get_bloginfo('name'); ?></title>
 
     <?php wp_head();?>
   </head>
