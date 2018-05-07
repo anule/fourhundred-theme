b7ac8a9de2514c48cd7e01ba53be8624f63d41e4
anule <anule.ndukwu@gmail.com>
2018-05-07 18:15:57 -0400

    move style.css to meet wp guidelines

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function fourhundred_scripts() {
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.1.1' );
     wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css' );
-    wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css' );
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '4.1.1', true );
     wp_enqueue_script( 'index', get_template_directory_uri() . '/js/index.js');
 }
